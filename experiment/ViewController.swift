1e6fe71c1740c5a609a1dbce8fed377976b7b5d2
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-20 00:57:52 +0100

    added a very simple application

--- experiment/ViewController.swift
+++ experiment/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mybutton: UIButton!
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var mylabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func myButtonPressed(_ sender: UIButton) {
+        print(sender.currentTitle!) // change text
+        print(textInput.text!) // any text that I introduced in the input text
+        mylabel.text = textInput.text!
+    }
 }
 
